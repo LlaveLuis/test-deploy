4d0d4c075eb2dff3829cc107db5d50d93751dc00
Luis Llave <llaveluis@gmail.com>
2019-09-05 18:48:39 -0500

    Add Profile model and update definition of User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User as UserBase
 from django.db import models
 
-# Create your models here.
+
+class User(UserBase):
+    """Customization to adding useful methods."""
+
+    class Meta:
+        proxy = True
+        ordering = ('first_name', 'last_name', )
+
+
+class Profile(models.Model):
+    """To store additional data for user."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, null=True, blank=True)
